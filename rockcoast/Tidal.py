ea3d656a80b730d9d7961604985f8aa611c16d71
Yuting Liu <yuting.liu3@studio.unibo.it>
2025-06-09 13:06:48 +0200

    Important processes

---
+++ rockcoast/Tidal.py
@@ -0,0 +1,80 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+# set up the geometry wher Z is elevation and X is cross-shore distance
+SLOPE = config['SLOPE']
+Z = np.arange(-10.,10.,0.1)
+X = Z/SLOPE
+SLInd = np.argmin(np.abs(0.-Z))
+
+""" SET THE TIDAL RANGE HERE (in metres) """
+TIDAL_RANGE = config['TIDAL_RANGE']
+
+# set up time vector and apply sine wave to show the tide.
+time = np.arange(0,24.,0.01)
+Zw = 0.5*TIDAL_RANGE*np.sin(time*2.*np.pi/12.)
+
+# set up a figure and set of axes for plotting
+fig3, ax3 = plt.subplots()
+
+# plot a tidal wave over a single day
+ax3.plot(time,Zw,'b-')
+ax3.set_xlabel("Time (hours)")
+ax3.set_ylabel("Water level (m)")
+
+fig4, ax4 = plt.subplots()
+ax4.hist(Zw, bins=20, orientation='horizontal', density=True)
+ax4.set_xlabel("Relative tidal duration (no units)")
+ax4.set_ylabel("Water level (m)")
+
+WAVEHEIGHT = config['WAVEHEIGHT']
+DECAY_COEFFICIENT = config['DECAY_COEFFICIENT']
+
+# set up an array to collect wave energy in
+CumulativeWaveHeight = np.zeros(len(Z))
+
+# reset water levels incase change of wave height
+Zw = 0.5 * TIDAL_RANGE * np.sin(time * 2. * np.pi / 12.)
+
+# set up a loop to consider each water level in the progressive position of the tide in turn
+for WaterLevel in Zw:
+    # find water level
+    WaterLevelInd = np.argmin(np.abs(WaterLevel - Z))
+
+    # calculate water depth and find breaking point
+    WaterDepth = WaterLevel - Z[0:WaterLevelInd]
+    BreakingPoint = np.argmin(np.abs(WaterDepth - WAVEHEIGHT * 0.8))
+
+    # calculate wave height from this point towards the coast
+    H = np.zeros(len(Z))
+    H[BreakingPoint:] = WAVEHEIGHT * np.exp(-(X[BreakingPoint:] - X[BreakingPoint]) * DECAY_COEFFICIENT)
+
+    # update the total wave energy array
+    CumulativeWaveHeight += H
+
+NormedWaveEnergy = CumulativeWaveHeight / np.max(CumulativeWaveHeight)
+
+# find high and low tide positions
+LowTideInd = np.argmin(np.abs(-0.5 * TIDAL_RANGE - Z))
+HighTideInd = np.argmin(np.abs(0.5 * TIDAL_RANGE - Z))
+
+# plot the results
+fig5, ax5 = plt.subplots()
+
+# plot the platform and mean sea level at 0m
+ax5.plot(X, Z, 'k-')
+ax5.plot([X[0], X[SLInd]], [0, 0], 'b--')
+ax5.plot([X[0], X[LowTideInd]], [-0.5 * TIDAL_RANGE, -0.5 * TIDAL_RANGE], 'b:')
+ax5.plot([X[0], X[HighTideInd]], [0.5 * TIDAL_RANGE, 0.5 * TIDAL_RANGE], 'b:')
+ax5.set_xlabel("Distance (m)")
+ax5.set_ylabel("Elevation (m)")
+
+sc = ax5.scatter(X, Z, s=NormedWaveEnergy * 100., c=CumulativeWaveHeight, zorder=10)
+cbar = plt.colorbar(sc)
+cbar.set_label("Relative Wave Energy (m)")
+
+ax5.set_xlim(-200, 100)
+plt.show()
--- rockcoast/wave.py
+++ rockcoast/wave.py
@@ -2,6 +2,10 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+'''
+Waves
+'''
 with open('config.json', 'r') as f:
     config = json.load(f)
 SLOPE = config['SLOPE']
@@ -26,9 +30,8 @@ ax1.set_ylabel("Elevation (m)")
 # ax1.axis('equal')
 # ax1.set_aspect(10.)
 
-plt.show()
 
-""" SET THE WAVE HEIGHT HERE """
+""" Wave Breaking """
 WAVEHEIGHT = config['WAVEHEIGHT']
 
 # calculate water depth and find breaking point
@@ -38,3 +41,27 @@ BreakingPoint = np.argmin(np.abs(WaterDepth-WAVEHEIGHT*0.8))
 #plot the breaking point
 ax1.plot([X[BreakingPoint],X[BreakingPoint]],[-WaterDepth[BreakingPoint],0],'r-')
 fig1
+
+""" WAVE DECAY"""
+DECAY_COEFFICIENT = config['DECAY_COEFFICIENT']
+COLOUR = 'b'
+
+# calculate wave heights
+H = np.zeros(SLInd)
+H[0:BreakingPoint] = WAVEHEIGHT
+H[BreakingPoint:] = WAVEHEIGHT*np.exp(-(X[BreakingPoint:SLInd]-X[BreakingPoint])*DECAY_COEFFICIENT)
+
+# set up a figure and set of axes for plotting
+fig2, ax2 = plt.subplots()
+
+ax2.plot(X[0:SLInd],H,'-',color=COLOUR)
+ax2.set_xlabel("Distance (m)")
+ax2.set_ylabel("Wave Height (m)")
+ax2.set_xlim(X[BreakingPoint]-10.,X[SLInd]+10.)
+
+
+# execute the exponential equation again here for the breaking wave
+H[BreakingPoint:] = WAVEHEIGHT*np.exp(-(X[BreakingPoint:SLInd]-X[BreakingPoint])*DECAY_COEFFICIENT)
+
+
+
